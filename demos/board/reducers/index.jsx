dde51cd5f17f2ae50ea22c558bc19ede3495465c
Marina Hoashi <marinahoashi@gmail.com>
2017-06-23 18:04:05 -0400

    Add sample board in html

---
+++ demos/board/reducers/index.jsx
@@ -0,0 +1,18 @@
+import {combineReducers} from 'redux'
+import {Map} from 'immutable'
+
+// -- // -- // Actions // -- // -- //
+
+
+// -- // -- // State // -- // -- //
+const initial = {
+
+}
+
+// -- // -- // Reducer // -- // -- //
+export default (state = initial, action) => {
+  // switch(action.type) {
+  //   case
+  // }
+  return state
+}
