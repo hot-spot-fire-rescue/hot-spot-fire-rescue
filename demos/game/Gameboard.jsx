2266129f313c6a7751f12fe713146eda5f273e7c
Yiling Jiang <yiling124@gmail.com>
2017-06-26 16:47:36 -0400

    half working

--- demos/game/Gameboard.jsx
+++ demos/game/Gameboard.jsx
@@ -1,7 +1,7 @@
 'use strict'
 import React from 'react'
 import {connect} from 'react-redux'
-
+import {switchDoor, switchWall} from './reducers/board'
 import {setupBoard} from './utils/setup'
 
 class Gameboard extends React.Component {
@@ -12,11 +12,19 @@ class Gameboard extends React.Component {
   }
 
   render() {
-    const {cells, boundaries, fetchInitialData} = this.props
+    const {players, cells, boundaries, fetchInitialData} = this.props
 
-    const alertWall = (event, coord) => {
+    const handleWallSwitch = (event, coord, status) => {
       event.stopPropagation()
-      alert(`this is a boundary with coordinates ${coord}`)
+      let newStatus = (status === 0) ? 1 : 0
+      console.log('wall~~~~~is about to be switched', coord, status, newStatus)
+      this.props.changeWallStatus(coord, newStatus)
+    }
+
+    const handleDoorSwitch = (event, coord, status) => {
+      let newStatus = (status === 0) ? 1 : 0
+      console.log('dooooor****** is about to be switched', coord, status, newStatus)
+      this.props.openCloseDoor(coord, newStatus)
     }
 
     const alertCell = (event, num) => {
@@ -39,7 +47,7 @@ class Gameboard extends React.Component {
               id={cell.number}
               onClick={(evt) => alertCell(evt, cell.number)}>
                 {
-                  player 
+                  player
                   ? <div className='player'
                     id={player.id} style={{backgroundColor: player.color}}/>
                   : null
@@ -48,42 +56,42 @@ class Gameboard extends React.Component {
                   eastBoundary && eastBoundary.kind === 'wall'
                   ? <div className='vertical-wall'
                     id={eastBoundaryCoord}
-                    onClick={(evt) => alertWall(evt, eastBoundaryCoord)} />
+                    onClick={(evt) => handleWallSwitch(evt, eastBoundaryCoord, eastBoundary.status)} />
                   : null
                 }
                 {
                   southBoundary && southBoundary.kind === 'wall'
                   ? <div className='horizontal-wall'
                     id={southBoundaryCoord}
-                    onClick={(evt) => alertWall(evt, southBoundaryCoord)} />
+                    onClick={(evt) => handleWallSwitch(evt, southBoundaryCoord)} />
                   : null
                 }
                 {
                   eastBoundary && eastBoundary.kind === 'door' && eastBoundary.status === 0
                   ? <div className='vertical-door-closed'
                     id={eastBoundaryCoord}
-                    onClick={(evt) => alertWall(evt, eastBoundaryCoord)} />
+                    onClick={(evt) => handleDoorSwitch(evt, eastBoundaryCoord, eastBoundary.status)} />
                   : null
                 }
                 {
                   southBoundary && southBoundary.kind === 'door' && southBoundary.status === 0
                   ? <div className='horizontal-door-closed'
                     id={southBoundaryCoord}
-                    onClick={(evt) => alertWall(evt, southBoundaryCoord)} />
+                    onClick={(evt) => handleDoorSwitch(evt, southBoundaryCoord, eastBoundary.status)} />
                   : null
                 }
                 {
                   eastBoundary && eastBoundary.kind === 'door' && eastBoundary.status === 1
                   ? <div className='vertical-door-open'
                     id={eastBoundaryCoord}
-                    onClick={(evt) => alertWall(evt, eastBoundaryCoord)} />
+                    onClick={(evt) => handleDoorSwitch(evt, eastBoundaryCoord, eastBoundary.status)} />
                   : null
                 }
                 {
                   southBoundary && southBoundary.kind === 'door' && southBoundary.status === 1
                   ? <div className='horizontal-door-open'
                     id={southBoundaryCoord}
-                    onClick={(evt) => alertWall(evt, southBoundaryCoord)} />
+                    onClick={(evt) => handleDoorSwitch(evt, southBoundaryCoord, southBoundary.status)} />
                   : null
                 }
               </div>
@@ -106,6 +114,12 @@ const mapState = ({board, player}) => ({
 const mapDispatch = dispatch => ({
   fetchInitialData: () => {
     dispatch(setupBoard())
+  },
+  openCloseDoor: (coord, status) => {
+    dispatch(switchDoor(coord, status))
+  },
+  changeWallStatus: (coord, status) => {
+    dispatch(switchWall(coord, status))
   }
 })
 
