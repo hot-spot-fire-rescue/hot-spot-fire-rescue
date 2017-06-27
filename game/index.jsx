a8d1c539f68171cb471309f1d7ac751481a23ec0
Marina Hoashi <marinahoashi@gmail.com>
2017-06-27 16:09:00 -0400

    Create link to test lobby from home page

--- game/index.jsx
+++ game/index.jsx
@@ -2,18 +2,17 @@
 import React from 'react'
 import {Route, IndexRedirect, IndexRoute, Link} from 'react-router'
 
+import Lobby from './components/Lobby'
 import GamePageWrapper from './GamePageWrapper'
 
-const Index = ({children}) => <div>
+// TODO: Show sign in component if not signed in, else show list of lobbies
+const HomePage = ({children}) => <div>
   <h1>Home Page</h1>
-
-  <h2><Link to='/game/test'>Start Game</Link></h2>
-  <p>
-    Our preliminary board for Hot Spot
-  </p>
+  <h2><Link to='/lobby/test'>Go to Test Lobby</Link></h2>
 </div>
 
 export default <Route path="/home" component={({children}) => children}>
-  <IndexRoute component={Index}/>
+  <IndexRoute component={HomePage}/>
+  <Route path='/lobby/:id' component={Lobby}/>
   <Route path='/game/:id' component={GamePageWrapper}/>
 </Route>
