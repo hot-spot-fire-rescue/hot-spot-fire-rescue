1fa1038a500ccbf2367341a312a2bf4c82a24719
Jing Wang <jwang2048@gmail.com>
2017-06-28 00:10:47 -0400

    added fire/smoke dumb component

--- game/reducers/index.jsx
+++ game/reducers/index.jsx
@@ -2,13 +2,9 @@ import {combineReducers} from 'redux'
 
 const rootReducer = combineReducers({
   board: require('./board').default,
-<<<<<<< HEAD:demos/game/reducers/index.jsx
-  player: require('./player').default,
   danger: require('./danger').default
-=======
   boundary: require('./boundary').default,
   player: require('./player').default
->>>>>>> 3242f02e29c57ace06576230f6dbfb4bfdb9c824:game/reducers/index.jsx
 })
 
 export default rootReducer
