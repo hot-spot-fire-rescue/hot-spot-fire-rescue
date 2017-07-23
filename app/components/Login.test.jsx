7a1982843a29b3dfd96db83f61c283e9599f5298
Marina Hoashi <marinahoashi@gmail.com>
2017-07-23 16:25:48 -0400

    Remove old tests

--- app/components/Login.test.jsx
+++ app/components/Login.test.jsx
@@ -18,10 +18,10 @@ describe('<Login />', () => {
     root = shallow(<Login auth={fakeAuth}/>)
   })
 
-  it('logs in with google', () => {
-    const button = root.find('button.google.login')
-    expect(button).to.have.length(1)
-    button.simulate('click')
-    expect(fakeAuth.signInWithPopup).to.have.been.calledWithMatch({providerId: 'google.com'})
-  })
+  // it('logs in with google', () => {
+  //   const button = root.find('button.google.login')
+  //   expect(button).to.have.length(1)
+  //   button.simulate('click')
+  //   expect(fakeAuth.signInWithPopup).to.have.been.calledWithMatch({providerId: 'google.com'})
+  // })
 })
